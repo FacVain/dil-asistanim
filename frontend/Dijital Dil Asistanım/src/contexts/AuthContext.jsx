54f3a678e34dfdbce1da3ec787752ccfb61b6604
halilerkan-cs <halilrkn61@gmail.com>
2024-02-04 17:36:49 +0300

    bug-fix: logout on refresh

--- frontend/Dijital Dil Asistanım/src/contexts/AuthContext.jsx
+++ frontend/Dijital Dil Asistanım/src/contexts/AuthContext.jsx
@@ -6,8 +6,10 @@ export const authReducer = (state, action) => {
   console.log("authReducer action: ", action);
   switch (action.type) {
     case "LOGIN":
+      localStorage.setItem("user", JSON.stringify(action.payload));
       return { ...state, user: action.payload };
     case "LOGOUT":
+      localStorage.removeItem("user");
       return { ...state, user: null };
     default:
       return state;
@@ -22,10 +24,29 @@ export const AuthProvider = ({ children }) => {
   console.log("AuthProvider state: ", state);
 
   useEffect(() => {
-    const user = localStorage.getItem("user");
-    if (user) {
-      dispatch({ type: "LOGIN", payload: JSON.parse(user) });
+    if (localStorage.getItem("user")) {
+      dispatch({
+        type: "LOGIN",
+        payload: JSON.parse(localStorage.getItem("user")),
+      });
+      return;
     }
+    axios
+      .get(`${import.meta.env.VITE_API_URL}/auth/login/success`, {
+        withCredentials: true,
+      })
+      .then((response) => {
+        if (response.status === 200) {
+          return response.data;
+        }
+        throw new Error("Authentication has been failed!");
+      })
+      .then((resObject) => {
+        dispatch({ type: "LOGIN", payload: resObject.user });
+      })
+      .catch((error) => {
+        console.error(error);
+      });
   }, []);
 
   return (
